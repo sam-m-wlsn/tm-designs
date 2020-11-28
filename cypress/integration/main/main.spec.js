90fb07c4d7d43faf4576972a9c9b07c4b9510960
sam-m-wlsn <sam.m.wlsn@gmail.com>
2020-11-28 16:50:04 -0600

    test(core): Add integration tests for main functionality.

--- cypress/integration/main/main.spec.js
+++ cypress/integration/main/main.spec.js
@@ -3,3 +3,30 @@
  * 
  * ToOo: break up into individual files per component as site gets larger.
  */
+context('core', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:3000/')
+  })
+
+  it('should display the splash page', () => {
+    cy.get('.marquee')
+      .should('be.visible')
+  })
+
+  it('should display gallery on scroll', () => {
+    cy.get('.marquee [data-splash-trigger]')
+      .click()
+
+    cy.get('#gallery')
+      .should('be.visible')
+  })
+
+  it('should display image at fullscreen on click', () => {    
+    cy.get('#gallery > ul > li:first-child')
+      .scrollIntoView()
+      .click()
+
+    cy.get('.image-gallery__item--active')
+      .should('be.visible')
+  })
+})
